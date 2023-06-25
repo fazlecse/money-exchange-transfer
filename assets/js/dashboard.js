73e393bd4c8ab575140b01ba7c8b59a5e3afc336
Md. Fazle Rabbi <mdfazlerabbi06@gmail.com>
2023-06-25 15:50:03 +0600

    first commit

---
+++ assets/js/dashboard.js
@@ -0,0 +1,322 @@
+
+// Preloader area
+var preloader = document.getElementById("preloader");
+function preloder_function() {
+    preloader.style.display = "none";
+}
+// toggleSideMenu start
+const toggleSideMenu = () => {
+    document.body.classList.toggle("toggle-sidebar");
+};
+// toggleSideMenu end
+
+
+(function () {
+    "use strict";
+
+    /**
+     * Easy selector helper function
+     */
+    const select = (el, all = false) => {
+        el = el.trim()
+        if (all) {
+            return [...document.querySelectorAll(el)]
+        } else {
+            return document.querySelector(el)
+        }
+    }
+
+    /**
+     * Easy event listener function
+     */
+    const on = (type, el, listener, all = false) => {
+        if (all) {
+            select(el, all).forEach(e => e.addEventListener(type, listener))
+        } else {
+            select(el, all).addEventListener(type, listener)
+        }
+    }
+
+    /**
+     * Easy on scroll event listener 
+     */
+    const onscroll = (el, listener) => {
+        el.addEventListener('scroll', listener)
+    }
+
+    /**
+     * Sidebar toggle
+     */
+    // if (select('.toggle-sidebar-btn')) {
+    //     on('click', '.toggle-sidebar-btn', function (e) {
+    //         select('body').classList.toggle('toggle-sidebar')
+    //     })
+    // }
+
+
+
+    // //  Search bar toggle
+
+    if (select('.search-bar-toggle')) {
+        on('click', '.search-bar-toggle', function (e) {
+            select('.search-bar').classList.toggle('search-bar-show')
+        })
+    }
+
+    /**
+     * Navbar links active state on scroll
+     */
+    let navbarlinks = select('#navbar .scrollto', true)
+    const navbarlinksActive = () => {
+        let position = window.scrollY + 200
+        navbarlinks.forEach(navbarlink => {
+            if (!navbarlink.hash) return
+            let section = select(navbarlink.hash)
+            if (!section) return
+            if (position >= section.offsetTop && position <= (section.offsetTop + section.offsetHeight)) {
+                navbarlink.classList.add('active')
+            } else {
+                navbarlink.classList.remove('active')
+            }
+        })
+    }
+    window.addEventListener('load', navbarlinksActive)
+    onscroll(document, navbarlinksActive)
+
+    /**
+     * Toggle .header-scrolled class to #header when page is scrolled
+     */
+    let selectHeader = select('#header')
+    if (selectHeader) {
+        const headerScrolled = () => {
+            if (window.scrollY > 100) {
+                selectHeader.classList.add('header-scrolled')
+            } else {
+                selectHeader.classList.remove('header-scrolled')
+            }
+        }
+        window.addEventListener('load', headerScrolled)
+        onscroll(document, headerScrolled)
+    }
+
+    /**
+     * Back to top button
+     */
+    let backtotop = select('.back-to-top')
+    if (backtotop) {
+        const toggleBacktotop = () => {
+            if (window.scrollY > 100) {
+                backtotop.classList.add('active')
+            } else {
+                backtotop.classList.remove('active')
+            }
+        }
+        window.addEventListener('load', toggleBacktotop)
+        onscroll(document, toggleBacktotop)
+    }
+
+    /**
+     * Initiate tooltips
+     */
+    var tooltipTriggerList = [].slice.call(document.querySelectorAll('[data-bs-toggle="tooltip"]'))
+    var tooltipList = tooltipTriggerList.map(function (tooltipTriggerEl) {
+        return new bootstrap.Tooltip(tooltipTriggerEl)
+    })
+
+    /**
+     * Initiate quill editors
+     */
+    if (select('.quill-editor-default')) {
+        new Quill('.quill-editor-default', {
+            theme: 'snow'
+        });
+    }
+
+    if (select('.quill-editor-bubble')) {
+        new Quill('.quill-editor-bubble', {
+            theme: 'bubble'
+        });
+    }
+
+    if (select('.quill-editor-full')) {
+        new Quill(".quill-editor-full", {
+            modules: {
+                toolbar: [
+                    [{
+                        font: []
+                    }, {
+                        size: []
+                    }],
+                    ["bold", "italic", "underline", "strike"],
+                    [{
+                        color: []
+                    },
+                    {
+                        background: []
+                    }
+                    ],
+                    [{
+                        script: "super"
+                    },
+                    {
+                        script: "sub"
+                    }
+                    ],
+                    [{
+                        list: "ordered"
+                    },
+                    {
+                        list: "bullet"
+                    },
+                    {
+                        indent: "-1"
+                    },
+                    {
+                        indent: "+1"
+                    }
+                    ],
+                    ["direction", {
+                        align: []
+                    }],
+                    ["link", "image", "video"],
+                    ["clean"]
+                ]
+            },
+            theme: "snow"
+        });
+    }
+
+    /**
+     * Initiate TinyMCE Editor
+     */
+    const useDarkMode = window.matchMedia('(prefers-color-scheme: dark)').matches;
+    const isSmallScreen = window.matchMedia('(max-width: 1023.5px)').matches;
+
+
+
+    /**
+     * Initiate Bootstrap validation check
+     */
+    var needsValidation = document.querySelectorAll('.needs-validation')
+
+    Array.prototype.slice.call(needsValidation)
+        .forEach(function (form) {
+            form.addEventListener('submit', function (event) {
+                if (!form.checkValidity()) {
+                    event.preventDefault()
+                    event.stopPropagation()
+                }
+
+                form.classList.add('was-validated')
+            }, false)
+        })
+
+    /**
+     * Initiate Datatables
+     */
+    const datatables = select('.datatable', true)
+    datatables.forEach(datatable => {
+        new simpleDatatables.DataTable(datatable);
+    })
+
+    /**
+     * Autoresize echart charts
+     */
+    const mainContainer = select('#main');
+    if (mainContainer) {
+        setTimeout(() => {
+            new ResizeObserver(function () {
+                select('.echart', true).forEach(getEchart => {
+                    echarts.getInstanceByDom(getEchart).resize();
+                })
+            }).observe(mainContainer);
+        }, 200);
+    }
+
+})();
+
+
+
+
+
+// input file preview
+const previewImage = (id) => {
+    document.getElementById(id).src = URL.createObjectURL(event.target.files[0]);
+};
+
+// Tooltip
+const tooltipTriggerList = document.querySelectorAll('[data-bs-toggle="tooltip"]')
+const tooltipList = [...tooltipTriggerList].map(tooltipTriggerEl => new bootstrap.Tooltip(tooltipTriggerEl));
+
+
+// cmn-select2
+$(document).ready(function () {
+    $('.cmn-select2-dropdown').select2();
+});
+$(".modal-select").select2({
+    dropdownParent: $("#formModal"),
+});
+
+// select2 with image
+function formatState(state) {
+    if (!state.id) {
+        return state.text;
+    }
+    var baseUrl = "../img/mini-flag";
+    var $state = $(
+        '<span><img src="' + baseUrl + '/' + state.element.value.toLowerCase() + '.svg" class="img-flag" /> ' + state.text + '</span>'
+    );
+    return $state;
+};
+// select2
+$(".js-example-basic-single").select2({
+    templateResult: formatState
+});
+
+
+
+
+$(document).ready(function () {
+    // owl carousel dashboard card
+    $('.carousel-1').owlCarousel({
+        loop: true,
+        autoplay: false,
+        margin: -20,
+        nav: false,
+        dots: false,
+        // rtl:true,
+        responsive: {
+            0: {
+                items: 1
+            },
+            576: {
+                items: 2
+            },
+            768: {
+                items: 3
+            }
+        }
+    });
+
+
+
+});
+
+
+
+// Dark theme start
+const toggleBtn = document.getElementById("toggle-btn");
+const body = document.querySelector("body");
+toggleBtn.addEventListener("click", function () {
+    document.body.classList.toggle("dark-theme");
+    if (document.body.classList.contains("dark-theme")) {
+        document.getElementById("moon").style.display = "none";
+        document.getElementById("sun").style.display = "block";
+    } else {
+        document.getElementById("moon").style.display = "block";
+        document.getElementById("sun").style.display = "none";
+    }
+});
+// Dark theme end
+
+
---
+++ assets/js/main.js
@@ -0,0 +1,298 @@
+// Preloader area
+const preloader = document.getElementById("preloader");
+function preloder_function() {
+    preloader.style.display = "none";
+}
+// toggleSideMenu start
+const toggleSideMenu = () => {
+    document.body.classList.toggle("toggle-sidebar");
+};
+// toggleSideMenu end
+
+// add bg to nav
+window.addEventListener("scroll", function () {
+    let scrollpos = window.scrollY;
+    const header = document.querySelector("nav");
+    const headerHeight = header.offsetHeight;
+
+    if (scrollpos >= headerHeight) {
+        header.classList.add("active");
+    } else {
+        header.classList.remove("active");
+    }
+});
+
+
+$(document).ready(function () {
+    // owl carousel dashboard card
+    $('.carousel-area1').owlCarousel({
+        loop: true,
+        autoplay: false,
+        margin: 10,
+        nav: true,
+        dots: false,
+        // rtl:true,
+        responsive: {
+            0: {
+                items: 1
+            },
+            400: {
+                items: 2
+            },
+            991: {
+                items: 2
+            }
+        }
+    });
+
+    // What's New carousel
+    $('.carousel-area2').owlCarousel({
+        loop: true,
+        autoplay: false,
+        margin: 20,
+        nav: true,
+        dots: false,
+        // rtl:true,
+        responsive: {
+            0: {
+                items: 1
+            },
+            575: {
+                items: 3
+            },
+            1000: {
+                items: 4
+            },
+            1400: {
+                items: 5
+            },
+            1620: {
+                items: 6
+            },
+        }
+    });
+    // Stream channel What's New carousel
+    $('.carousel-area3').owlCarousel({
+        loop: true,
+        autoplay: false,
+        margin: 20,
+        nav: true,
+        dots: false,
+        // rtl:true,
+        responsive: {
+            0: {
+                items: 1
+            },
+            400: {
+                items: 2
+            },
+            600: {
+                items: 3
+            },
+            800: {
+                items: 4
+            },
+            1000: {
+                items: 4
+            }
+        }
+    });
+    // responsive-filter
+    $('.carousel-area4').owlCarousel({
+        loop: true,
+        autoplay: false,
+        margin: 20,
+        nav: true,
+        dots: false,
+        // rtl:true,
+        responsive: {
+            0: {
+                items: 1
+            },
+            420: {
+                items: 2
+            },
+            550: {
+                items: 3
+            },
+            720: {
+                items: 4
+            },
+        }
+    });
+
+    // Swiper-slider livestream-swiper
+    var swiper = new Swiper(".livestream-swiper", {
+        effect: "coverflow",
+        grabCursor: true,
+        centeredSlides: true,
+        slidesPerView: "auto",
+        coverflowEffect: {
+            rotate: 0,
+            stretch: 0,
+            depth: 100,
+            modifier: 1,
+            slideShadows: true,
+        },
+        // Navigation arrows
+        navigation: {
+            nextEl: '.swiper-button-next',
+            prevEl: '.swiper-button-prev',
+        },
+        loop: true,
+
+    });
+    // Testimonial section start
+    // Owl carousel 
+    $(function (e) {
+        "use strict";
+        $('.testimonial-carousel').owlCarousel({
+            loop: true,
+            autoplay: false,
+            center: true,
+            margin: 10,
+            nav: false,
+            dots: true,
+            rtl: true,
+            responsive: {
+                0: {
+                    items: 1
+                },
+                600: {
+                    items: 2
+                },
+                1000: {
+                    items: 3
+                }
+            }
+        });
+    });
+    // Testimonial section start
+
+
+    // noUiSlider1 start pricing plan
+    var nonLinearSlider = document.getElementById('slider');
+    noUiSlider.create(nonLinearSlider, {
+        connect: true,
+        behaviour: 'tap',
+        start: [10],
+        range: {
+            'min': [0],
+            'max': [100]
+        }
+    });
+    var nodes = [
+        document.getElementById('slider-value'),
+    ];
+
+    // Display the slider value and how far the handle moved
+    // from the left edge of the slider.
+    nonLinearSlider.noUiSlider.on('update', function (values, handle, unencoded, isTap, positions) {
+        nodes[handle].innerHTML = values[handle] + ', ';
+    });
+    // noUiSlider end
+
+
+
+
+    // cmn-select2
+    $(document).ready(function () {
+        $('.cmn-select2').select2({
+            templateResult: formatState,
+            templateSelection: formatState
+
+        });
+    });
+    // select2 with image
+    function formatState(state) {
+        if (!state.id) {
+            return state.text;
+        }
+        var baseUrl = "assets/img/mini-flag";
+        var $state = $(
+            '<span><img src="' + baseUrl + '/' + state.element.value.toLowerCase() + '.svg" class="img-flag" /> ' + state.text + '</span>'
+        );
+        return $state;
+    };
+    function formatState (state) {
+        if (!state.id) {
+          return state.text;
+        }
+      
+        var baseUrl = "assets/img/mini-flag";
+        var $state = $(
+          '<span><img class="img-flag" /> <span></span></span>'
+        );
+      
+        // Use .text() instead of HTML string concatenation to avoid script injection issues
+        $state.find("span").text(state.text);
+        $state.find("img").attr("src", baseUrl + "/" + state.element.value.toLowerCase() + ".svg");
+      
+        return $state;
+      };
+    $(".modal-select").select2({
+        dropdownParent: $("#formModal"),
+    });
+
+
+
+});
+
+
+
+// input file preview
+const previewImage = (id) => {
+    document.getElementById(id).src = URL.createObjectURL(event.target.files[0]);
+};
+
+// Tooltip
+const tooltipTriggerList = document.querySelectorAll('[data-bs-toggle="tooltip"]')
+const tooltipList = [...tooltipTriggerList].map(tooltipTriggerEl => new bootstrap.Tooltip(tooltipTriggerEl));
+
+// Copy text start
+function copyTextFunc() {
+    // get the container
+    const element = document.querySelector('.docs-code');
+    // Create a fake `textarea` and set the contents to the text
+    // you want to copy
+    const storage = document.createElement('textarea');
+    storage.value = element.innerHTML;
+    element.appendChild(storage);
+
+    // Copy the text in the fake `textarea` and remove the `textarea`
+    storage.select();
+    storage.setSelectionRange(0, 99999);
+    document.execCommand('copy');
+    element.removeChild(storage);
+}
+// Copy text end
+// Highlight editor start
+document.addEventListener('DOMContentLoaded', (event) => {
+    document.querySelectorAll('pre code').forEach((el) => {
+        hljs.highlightElement(el);
+    });
+});
+// Highlight editor end
+
+// input field show hide password start
+const password = document.querySelector('.password');
+const passwordIcon = document.querySelector('.password-icon');
+
+passwordIcon.addEventListener("click", function () {
+    if (password.type == 'password') {
+        password.type = 'text';
+        passwordIcon.classList.add('fa-eye-slash');
+    } else {
+        password.type = 'password';
+        passwordIcon.classList.remove('fa-eye-slash');
+    }
+})
+// input field show hide password end
+
+// Int Tel Input With Flags and Dial Codes start
+const input = document.querySelector("#telephone");
+window.intlTelInput(input, {
+    initialCountry: "bd",
+    separateDialCode: true,
+});
+// Int Tel Input With Flags and Dial Codes end
